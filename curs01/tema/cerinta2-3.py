7fefb1c0a411dc372a110e46e371b0c57298f87e
carinapadurariu <carinaflorina113@gmail.com>
2022-11-27 21:29:24 +0200

    curs1

---
+++ curs01/tema/cerinta2-3.py
@@ -0,0 +1,10 @@
+lista = [2,3,5,6,12,17,23]
+lista.sort()
+print(lista)
+
+
+# cerinta3
+
+lista1 = [1,12,34,56,87]
+lista1.reverse()
+print(lista1)
---
+++ curs01/tema/cerinta_6.py
@@ -0,0 +1,4 @@
+list1=[1,7,32,76,53,99,79]
+for num in list1:
+    if num % 2 != 0:
+        print(num, end=" ")
